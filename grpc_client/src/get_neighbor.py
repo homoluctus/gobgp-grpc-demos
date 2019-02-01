6aca74dc883ff1837044a84ffe0506b2f6a91694
homoluctus <w.slife18sy@gmail.com>
2019-02-01 14:13:53 +0900

    Move Client class to client.py

--- grpc_client/src/get_neighbor.py
+++ grpc_client/src/get_neighbor.py
@@ -1,35 +1,5 @@
-import grpc
 import gobgp_pb2
-import gobgp_pb2_grpc
-
-
-class Client(object):
-    def __init__(self, address):
-        """
-        Support context manager
-        
-        :params address: Bind host address and port
-        """
-
-        try:
-            self.__channel = grpc.insecure_channel(address)
-            self.__stub = gobgp_pb2_grpc.GobgpApiStub(self.__channel)
-        except grpc.RpcError:
-            raise
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, *exc_details):
-        return self.__channel.close()
-
-    @property
-    def channel(self):
-        return self.__channel
-
-    @property
-    def stub(self):
-        return self.__stub
+from client import Client
 
 
 def get_peers_by_address(stub, address):
