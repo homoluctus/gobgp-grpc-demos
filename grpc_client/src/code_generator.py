d4ebf9fcefc7d2e9de8c3c19e2d1459d9c073627
homoluctus <w.slife18sy@gmail.com>
2019-02-02 16:33:52 +0900

    Add google/protbuf

--- grpc_client/src/code_generator.py
+++ grpc_client/src/code_generator.py
@@ -5,9 +5,12 @@ protoc.main((
     '',
     '-I../protobuf/',
     '-I../proto',
-    '--python_out=./gobgp',
+    '--python_out=.',
     '--grpc_python_out=.',
     '../proto/gobgp.proto',
     '../proto/attribute.proto',
-    '../proto/capability.proto'
+    '../proto/capability.proto',
+    '../protobuf/google/protobuf/any.proto',
+    '../protobuf/google/protobuf/empty.proto',
+    '../protobuf/google/protobuf/timestamp.proto',
 ))
