9d1f59013e23a0f2b4c198ae00c183e57d16a608
homoluctus <w.slife18sy@gmail.com>
2019-02-06 02:36:09 +0900

    Add PathHandler class includes get_path and add_path functions

--- grpc_client/src/add_path.py
+++ grpc_client/src/add_path.py
@@ -1,56 +1,82 @@
 import binascii
 from gobgp import gobgp_pb2, attribute_pb2
 from google.protobuf import any_pb2
-from client import run
+
+import client
 
 
-def add_path(stub, table_type, path, vrf_id=None):
-    request = gobgp_pb2.AddPathRequest(
-                table_type=table_type,
-                vrf_id=vrf_id,
-                path=path)
-    
-    response = stub.AddPath(request)
+class PathHandler(client.GoBGPClient):
+    def __init__(self, server_address, table_type, family, vrf_id=None):
+        super().__init__(server_address)
+        self.table_type = table_type
+        self.family = family
+        self.vrf_id = vrf_id
 
-    if response.uuid:
-        print('SUCCESS')
-    else:
-        print('FAILURE')
+    def get_path(self, neighbor_ip=None, prefixes=None, sort=True):
+        request = gobgp_pb2.ListPathRequest(
+            table_type=self.table_type,
+            name=neighbor_ip,
+            family=self.family,
+            prefixes=prefixes,
+            sort_type=sort
+        )
 
+        list_path = []
+        for response in self.stub.ListPath(request):
+            list_path.append(response.destination)
 
-def main():
-    table_type = gobgp_pb2.GLOBAL
+        return list_path
 
-    nlri = any_pb2.Any()
-    nlri.Pack(attribute_pb2.IPAddressPrefix(
-        prefix_len=24,
-        prefix='192.0.2.0'
-    ))
-
-    # attributes
-    origin = any_pb2.Any()
-    origin.Pack(attribute_pb2.OriginAttribute(origin=0))
-
-    next_hop = any_pb2.Any()
-    next_hop.Pack(attribute_pb2.NextHopAttribute(next_hop='0.0.0.1'))
-
-    no_export = 4294967041
-    communities = any_pb2.Any()
-    communities.Pack(attribute_pb2.CommunitiesAttribute(communities=[no_export]))
-
-    pattrs = [origin, next_hop, communities]
-
-    path = gobgp_pb2.Path(
+    def add_path(self, nlri, path_attributes=None, is_nexthop_invalid=False):
+        request = gobgp_pb2.AddPathRequest(
+            table_type=self.table_type,
+            vrf_id=self.vrf_id,
+            path=gobgp_pb2.Path(
                 nlri=nlri,
-                pattrs=pattrs,
-                family=gobgp_pb2.Family(afi=1, safi=1),
-                is_nexthop_invalid=True,
+                pattrs=path_attributes,
+                family=self.family,
+                is_nexthop_invalid=is_nexthop_invalid,
             )
+        )
 
-    run(server_address='198.51.100.1:50051',
-        callback=add_path,
-        args=(table_type, path))
+        if self.stub.AddPath(request):
+            return True
+        else:
+            return False
 
 
 if __name__ == '__main__':
-    main()
+    with PathHandler(server_address='198.51.100.1:50051',
+                     table_type=gobgp_pb2.GLOBAL,
+                     family=gobgp_pb2.Family(afi=1, safi=1)) as client:
+
+        nlri = any_pb2.Any()
+        nlri.Pack(attribute_pb2.IPAddressPrefix(
+            prefix_len=24,
+            prefix='192.0.2.0'
+        ))
+
+        # attributes
+        origin = any_pb2.Any()
+        origin.Pack(attribute_pb2.OriginAttribute(origin=0))
+
+        next_hop = any_pb2.Any()
+        next_hop.Pack(attribute_pb2.NextHopAttribute(next_hop='0.0.0.2'))
+
+        no_export = 4294967041
+        communities = any_pb2.Any()
+        communities.Pack(attribute_pb2.CommunitiesAttribute(communities=[no_export]))
+
+        pattrs = [origin, next_hop, communities]
+        
+        ret = client.add_path(
+            nlri=nlri,
+            path_attributes=pattrs,
+            is_nexthop_invalid=True,
+        )
+
+        if ret:
+            print('SUCESS\n')
+            print(client.get_path())
+        else:
+            print('FAILURE')
