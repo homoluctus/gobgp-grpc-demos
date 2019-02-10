f9a0b512e9b0e27c2ed16a9e515b7d4342e5f15a
homoluctus <w.slife18sy@gmail.com>
2019-02-11 00:52:39 +0900

    Add to format json from proto message

--- grpc_client/src/path.py
+++ grpc_client/src/path.py
@@ -1,14 +1,17 @@
 from gobgp import gobgp_pb2, attribute_pb2
+from google.protobuf.json_format import MessageToJson, Parse
 
 
 class PathHandler:
-    def __init__(self, stub, table_type, family, vrf_id=None):
+    def __init__(self, stub, table_type, family,
+                 vrf_id=None):
         self.__stub = stub
         self.table_type = table_type
         self.family = family
         self.vrf_id = vrf_id
 
-    def get_path(self, neighbor_ip=None, prefixes=None, sort=True):
+    def get_path(self, neighbor_ip=None, prefixes=None,
+                 sort=True, json_format=True):
         request = gobgp_pb2.ListPathRequest(
             table_type=self.table_type,
             name=neighbor_ip,
@@ -17,11 +20,13 @@ class PathHandler:
             sort_type=sort
         )
 
-        list_path = []
         for response in self.__stub.ListPath(request):
-            list_path.append(response)
-
-        return list_path
+            if json_format:
+                yield MessageToJson(
+                        response,
+                        including_default_value_fields=True)
+            else:
+                yield response
 
     def add_path(self, nlri, path_attributes=None, is_nexthop_invalid=False,
                  source_asn=None, source_id=None):
