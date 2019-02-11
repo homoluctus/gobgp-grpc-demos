56bb0954b6874d62468704ab400e11167ad654d6
homoluctus <w.slife18sy@gmail.com>
2019-02-12 08:40:45 +0900

    Modify to use run() in client module

--- grpc_client/src/get_path.py
+++ grpc_client/src/get_path.py
@@ -1,12 +1,18 @@
 from gobgp import gobgp_pb2
-from client import GoBGPClient
+from client import run
 from path import PathHandler
 
 
-if __name__ == '__main__':
-    with GoBGPClient(server_address='198.51.100.1:50051') as client:
-        path_handler = PathHandler(client.stub,
-                                   table_type=gobgp_pb2.GLOBAL,
-                                   family=gobgp_pb2.Family(afi=1, safi=1))
+def get_path(stub, table_type, family):
+    path_handler = PathHandler(stub,
+                               table_type=table_type,
+                               family=family)
 
-        print(path_handler.get_path())
+    for destination in path_handler.get_path():
+        print(destination)
+
+
+if __name__ == '__main__':
+    run(server_address='198.51.100.1:50051',
+        callback=get_path,
+        args=(gobgp_pb2.GLOBAL, gobgp_pb2.Family(afi=1, safi=1)))
